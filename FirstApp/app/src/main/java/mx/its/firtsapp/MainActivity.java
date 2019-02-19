689b8f3e2265176cd4a07717b895fa64163565fa
Ing. Alejandro Soto <soto@itsaltillo.edu.mx>
2019-02-18 18:01:28 -0600

    Listeners

--- FirstApp/app/src/main/java/mx/its/firtsapp/MainActivity.java
+++ FirstApp/app/src/main/java/mx/its/firtsapp/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     private EditText txtNombre;
     private RatingBar rating;
     private Button button1,button2;
@@ -27,9 +27,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rating =  findViewById(R.id.rating);
         logo = findViewById(R.id.logo);
         seek = findViewById(R.id.seek);
-
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
+        //rating.setRating(5.0f);
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.rating_selected,rating),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(getApplicationContext(),
+                        " El progreso cambió a :"+progress,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),
+                        " Inicia desplazamiento :",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),
+                        " Termina desplazamiento",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        logo.setOnClickListener(this);
+
     }
 
     @Override
@@ -39,6 +71,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if(v.getId()== R.id.button2){
             Toast.makeText(getApplicationContext(),"Hiciste click en mujer",Toast.LENGTH_SHORT).show();
+        }
+        if(v.getId()== R.id.logo){
+            if(txtNombre.getText().toString().isEmpty()){
+                Toast.makeText(getApplicationContext(),
+                        "Escribe tu nombre primero!",
+                        Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(),
+                        " Hola, "+txtNombre.getText().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
